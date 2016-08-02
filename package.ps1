f8a1e66f57203f77c2038e5d3593670198172213
Tamar Christina <tamar@zhox.com>
2016-08-02 22:24:22 +0100

    GhcDevel: Fix packing script

--- package.ps1
+++ package.ps1
@@ -8,27 +8,26 @@ Write-Host "$bin"
 ls ghc-devel*.nuspec -recurse -File | ForEach-Object { 
             $src = (Split-Path $_.FullName -Parent)
             $tmp = Join-Path $bin "tmp_build"
-            New-Item $tmp
+            $base = $_.BaseName
+            New-Item $tmp -Type Directory -Force | Out-Null
 
             Write-Host "Packaging $_"
             
             # Copy the files over
-            Copy-Item -Recurse -Force -Path $src -Include * -Destination $tmp
-            Copy-Item -Recurse -Force -Path $templates -Include * -Destination $tmp
-            Copy-Item -Recurse -Force -Path $compat -Include * -Destination $tmp
-            
-            Remove-Item -Path $tmp -Filter *.nupkg
-            
+            Copy-Item -Recurse -Force -Path (Join-Path $src "*") -Destination $tmp
+            Copy-Item -Recurse -Force -Path (Join-Path $templates "*") -Destination $tmp
+            Copy-Item -Recurse -Force -Path (Join-Path $compat "*") -Destination $tmp
+
+            Remove-Item -Path $tmp -Include *.nuspec
+
             $source = $tmp
-            $destination = Join-Path $bin ($_.BaseName + '.zip')
+            $destination = Join-Path $bin ($base + '.zip')
 
             If(Test-path $destination) {Remove-item $destination}
             [io.compression.zipfile]::CreateFromDirectory($Source, $destination) 
-            
-            cd $dir
-            choco pack
+
             Remove-Item -Path $tmp -Recurse -Force
-            Write-Host "Done packging $_.BaseName"
+            Write-Host "Done packaging $base"
         }
         
 cd $path
